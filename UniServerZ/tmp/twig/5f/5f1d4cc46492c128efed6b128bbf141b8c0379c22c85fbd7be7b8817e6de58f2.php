5ace3205860d60715d92246fdca6d5461749b730
Kieran Gara <kierangara@gmail.com>
2024-05-23 00:41:18 -0400

    Environment setup complete

---
+++ UniServerZ/tmp/twig/5f/5f1d4cc46492c128efed6b128bbf141b8c0379c22c85fbd7be7b8817e6de58f2.php
@@ -0,0 +1,62 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* display/results/empty_display.twig */
+class __TwigTemplate_064f2c7fd8081e37fbecb2b51fa9d480d95c7b1eb87672a4ef02a637c37b8bfa extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<td class=\"";
+        echo twig_escape_filter($this->env, ($context["classes"] ?? null), "html", null, true);
+        echo "\"></td>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "display/results/empty_display.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "display/results/empty_display.twig", "C:\\Users\\Kieran\\TFSServer\\UniServerZ\\home\\us_opt1\\templates\\display\\results\\empty_display.twig");
+    }
+}
